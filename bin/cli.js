71e4b79961ad59580b9c0999fc8d5866e395919d
kaival patel <kaival@email.unc.edu>
2022-11-15 14:48:33 -0500

    added default to cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -5,8 +5,8 @@ import { roll } from "../lib/roll.js";
 
 const args = minimist(process.argv.slice(2));
 
-var side = args.sides;
-var dice = args.dice;
-var rolls = args.rolls;
+var side = args.sides || 6;
+var dice = args.dice || 2;
+var rolls = args.rolls || 1;
 
 console.log(JSON.stringify(roll(side, dice, rolls)));
